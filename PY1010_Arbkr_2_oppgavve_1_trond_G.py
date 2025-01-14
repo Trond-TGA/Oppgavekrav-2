b1f80eb5ac3c09f3ca52d36f368e8eaeb033c6b4
Trond-TGA <gabbentro@gmail.com>
2025-01-14 14:41:39 +0100

    Update PY1010_Arbkr_2_oppgavve_1_trond_G.py
    Korrigerer noen kommentarer til koden

--- PY1010_Arbkr_2_oppgavve_1_trond_G.py
+++ PY1010_Arbkr_2_oppgavve_1_trond_G.py
@@ -5,19 +5,20 @@ Oppgave 1 arbeidskrav 2
 @author: Trond G
 """
 
+#%% """Henter inn moduler"""
 import numpy as np
 import time
 import datetime
 
-
-#%% "" utregninger"""
+#%% """ utregninger"""
 aarfodt = int(input("Hvilket år ble du født?", ))
 
+#%% henter tidenstempel fra datamaskinen som brukes
 referansetid = time.localtime()
 
 AlderIaar =  referansetid.tm_year - aarfodt +1
 
-#%% """ut på skjermen"""
+#%% """ut på skjermen Her vil utregningene være feil hvis bruker har hatt bursdag i løpet av dette året"""
 
 print ( "Du er ", referansetid.tm_year - aarfodt,"år gammel, og holder deg godt :-)")
 print ( "Dersom alt går som planlagt, vil du bli ", AlderIaar,"i år.")
